0c71d01aa7152bc2dece46465ba86868b71438ef
Nicolas Ferré <Nicolas-Ferre@users.noreply.github.com>
2025-11-27 19:41:37 +0100

    Add Zig project skeleton (#16)

---
+++ src/main.zig
@@ -0,0 +1,14 @@
+const std = @import("std");
+
+pub fn main() !void {
+    std.debug.print("Double of 2 is {}\n", .{double(2)});
+}
+
+fn double(number: i32) i32 {
+    return number * 2;
+}
+
+test "dummy test" {
+    try std.testing.expectEqual(0, double(0));
+    try std.testing.expectEqual(4, double(2));
+}
