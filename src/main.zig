366cf6555a3a9bd3199ad63eb139dfad42d15098
Nicolas Ferré <Nicolas-Ferre@users.noreply.github.com>
2025-11-30 15:42:30 +0100

    Add file reading utils (#32)

--- src/main.zig
+++ src/main.zig
@@ -1,14 +1,9 @@
 const std = @import("std");
 
 pub fn main() void {
-    std.debug.print("Double of 2 is {}\n", .{double(2)});
+    std.debug.print("Command not yet implemented\n", .{});
 }
 
-fn double(number: i32) i32 {
-    return number * 2;
-}
-
-test "dummy test" {
-    try std.testing.expectEqual(0, double(0));
-    try std.testing.expectEqual(4, double(2));
+test {
+    _ = @import("compiler/reading.zig");
 }
